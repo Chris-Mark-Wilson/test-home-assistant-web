c6564fea3c44d869dcec8de0e85029f49a311952
Chris-Mark-Wilson <chris@cmwilson.co.uk>
2024-07-12 23:32:19 +0100

    removed data dump

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { getEndpoint } from "./api";
-import "../test.css"
+import "../test.css";
 
 export const Test = () => {
   const [data, setData] = useState(["No data"]);
@@ -10,7 +10,7 @@ export const Test = () => {
     try {
       const response = await getEndpoint(endpoint);
       if (Array.isArray(response)) {
-        console.log('array received')
+        console.log("array received");
         setData(response);
       } else setData([response]);
     } catch (err) {
@@ -26,42 +26,51 @@ export const Test = () => {
       <button onClick={() => sendRequest("services")}>/api/services</button> */}
       <button onClick={() => sendRequest("states")}>/api/states</button>
       <ol className="data-list">
-        {data.map((item,index) => {
+        {data.map((item, index) => {
           const headers = Object.keys(item);
           const values = Object.values(item);
-        //   console.log('headers: ',headers);
-        //   console.log('values: ',values)
+          //   console.log('headers: ',headers);
+          //   console.log('values: ',values)
           return (
             <>
-            <hr/><br/>
-            <div>State Object: {index}</div>
-            <ul className="data-list-item" key={index}>
-              {headers.map((header, index2) => {
-          
-                if(typeof (values[index2])=="object"){
+              <hr />
+              <br />
+              <div>State Object: {index}</div>
+              <ul className="data-list-item" key={index}>
+                {headers.map((header, index2) => {
+                  if (typeof values[index2] == "object") {
                     // console.log("here")
                     return (
-                        <ol className="sub-data-list-item" key={index2}>
-                            {Object.values(values[index2]).map((value,index3)=>{
-                                return(
-                                    <li key={index3}><div>{Object.keys(values[index2])[index3].toUpperCase()}:</div> <div>{value??"Null"}</div></li>
-                                )
-                            })}
-                            </ol>
-                    )
-                } else
-                return (
-                  <li key={index2}>
-                    <div>{header.toUpperCase()}:</div> <div>{values[index2]??"Null"}</div>
-                  </li>
-                );
-              })}
-            </ul>
+                      <ol className="sub-data-list-item" key={index2}>
+                        {Object.values(values[index2]).map((value, index3) => {
+                          return (
+                            <li key={index3}>
+                              <div>
+                                {Object.keys(values[index2])[
+                                  index3
+                                ].toUpperCase()}
+                                :
+                              </div>{" "}
+                              <div>{value ?? "Null"}</div>
+                            </li>
+                          );
+                        })}
+                      </ol>
+                    );
+                  } else
+                    return (
+                      <li key={index2}>
+                        <div>{header.toUpperCase()}:</div>{" "}
+                        <div>{values[index2] ?? "Null"}</div>
+                      </li>
+                    );
+                })}
+              </ul>
             </>
           );
         })}
       </ol>
-      <div className="data">{JSON.stringify(data)}</div>
+      {/* <div className="data">{JSON.stringify(data)}</div> */}
     </>
   );
 };
